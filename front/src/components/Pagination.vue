48861df4e43d7eeed8922a21d4f11730989815fc
cprice <cprice@tutanota.com>
2018-04-03 08:53:02 +0200

    [front] add raw component for layout

---
+++ front/src/components/Pagination.vue
@@ -0,0 +1,6 @@
+<template>
+  <ul class="actions pagination">
+    <li><a href="" class="disabled button big previous">Previous Page</a></li>
+    <li><a href="#" class="button big next">Next Page</a></li>
+  </ul>
+</template>
