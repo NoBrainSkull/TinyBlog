803f5e9ba448ed7dfe803bf06dcb8cccc0e8d6f6
cprice <cprice@tutanota.com>
2018-04-11 13:17:09 +0200

    [recommendations] dynamize

--- front/src/components/MiniPost.vue
+++ front/src/components/MiniPost.vue
@@ -1,14 +1,14 @@
 <template>
   <article>
     <header>
-      <v-card hover flat color="white" class="mini-post mt-5">
+      <v-card hover flat color="white" class="mini-post">
         <v-layout row wrap align-center justify-center>
           <div class="image">
-            <img :src="illustration" alt="" />
+            <img :src="post.illustration" alt="" />
           </div>
           <v-flex xs10 class="mt-3 px-3 py-3">
-            <h3 class="title raleway">{{ title }}</h3>
-            <time class="subheading" :datetime="createdAt">{{ createdAt }}</time>
+            <h3 class="title raleway">{{ post.title }}</h3>
+            <time class="subheading" :datetime="post.createdAt">{{ date2Str(post.created_at) }}</time>
           </v-flex>
           <v-flex xs2><v-avatar :size="40"><img :src="avatar" alt="" /></v-avatar></v-flex>
         </v-layout>
@@ -28,24 +28,23 @@
 </style>
 
 <script>
+  import calendar from '~/modules/dateUtils'
   export default {
     props: {
-      title: {
-        type: String,
-        required: true
-      },
-      createdAt: {
-        type: Date,
-        required: true
-      },
-      illustration: {
-        type: String,
+      post: {
+        type: Object,
         required: true
       },
       avatar: {
         type: String,
         required: true
       }
-    }
+    },
+    methods: {
+      date2Str: datetime => {
+        const date = calendar(datetime)
+        return `${date.monthName} ${date.day}, ${date.year}`
+      }
+    },
   }
 </script>
